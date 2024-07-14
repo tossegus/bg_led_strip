6a9b2e47ed10cff99c2e7183983b1696d3c124c5
Gustaf Bengtz <gbengtz@gmail.com>
2024-07-14 14:42:42 +0200

    WS2182b and ESP8266 BG project. Initial commit

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#ifndef _SECRETS_H
+#define _SECRETS_H
+
+#define SECRET_SSID "<your ssid>"
+#define SECRET_PSWD "<your wifi password>"
+#define SECRET_HOMEPAGE "<your nightscout url>"
+#define SECRET_TOKEN "<your nightscout token>"
+
+#endif
